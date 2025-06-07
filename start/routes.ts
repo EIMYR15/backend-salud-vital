b373906d778fd84ca92b27099ee6ad2f6e32fb0c
EIMYR15 <loren.1518@hotmail.com>
2025-06-07 11:41:07 -0500

    se agrega controlador y rutas

--- start/routes.ts
+++ start/routes.ts
@@ -1,16 +1,22 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import router from '@adonisjs/core/services/router'
 
+const controllerEspecialistas = () => import('#controllers/especialistas_controller')
+
+// Ruta de prueba
 router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
+  return { hello: 'world' }
 })
+
+// CRUD de especialistas
+router.get('/especialistas', [controllerEspecialistas, 'index'])
+router.post('/especialistas', [controllerEspecialistas, 'store'])
+router.get('/especialistas/:id', [controllerEspecialistas, 'show'])
+router.put('/especialistas/:id', [controllerEspecialistas, 'update'])
+router.delete('/especialistas/:id', [controllerEspecialistas, 'destroy'])
+
+// Restaurar especialista (soft delete)
+router.post('/especialistas/:id/restore', [controllerEspecialistas, 'restore'])
+
+// Eliminar definitivamente
+router.delete('/especialistas/:id/force', [controllerEspecialistas, 'forceDelete'])
+
